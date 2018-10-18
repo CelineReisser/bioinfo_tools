f3591561edd3ecdbfebcee5f55913addeedb5d58
cmor2207 <celine.reisser@gmail.com>
2018-10-18 13:31:32 -1000

    Update and rename homozygous_from_VCF.sh to Hom_Het_selection_from_VCF.sh

---
+++ Hom_Het_selection_from_VCF.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#PBS -q mpi
+#PBS -l walltime=24:00:00
+#PBS -l select=1:ncpus=28:mem=115g
+#PBS -o /home1/datawork/creisser/sex_det/98_log_files
+#PBS -N get_homozygotes
+
+DATADIRECTORY=/home1/scratch/creisser/sex_det/07_freebayes
+INDIR=/home1/scratch/creisser/sex_det/07_freebayes
+
+# Here we focused on a particular individual in column 12, for which we wanted to select only homozygous sites:
+
+cat $INDIR/sex_det_snp_DP10_maf0.1_miss0.9.recode.vcf | awk '$12 ~ "0/0:" ' > $INDIR/sex_det_snp_DP10_maf0.1_miss0.hom00.vcf
+cat $INDIR/sex_det_snp_DP10_maf0.1_miss0.9.recode.vcf | awk '$12 ~ "1/1:" ' > $INDIR/sex_det_snp_DP10_maf0.1_miss0.hom11.vcf
+
+cat $INDIR/sex_det_snp_DP10_maf0.1_miss0.9.recode.vcf | grep "#" > $INDIR/vcf.header.txt
+
+cat $INDIR/vcf.header.txt $INDIR/sex_det_snp_DP10_maf0.1_miss0.hom00.vcf $INDIR/sex_det_snp_DP10_maf0.1_miss0.hom11.vcf > $INDIR/sex_det_snp_DP10_maf0.1_miss0.9.final.vcf
+
+
+
+#selecting stricly heterozygous individuals for the rest of the dataset (selection made on multiple individuals at once)
+
+cat 'H:\WORK\SEX-DET_Pmarg\WGS\results\GWAS\sex_det_snp_DP10_maf0.1_miss0.final_biallelic.vcf' | awk '$10 ~ "0/1" ' | awk '$11 ~ "0/1" ' | awk '$13 ~ "0/1" ' | awk '$14 ~ "0/1" ' | awk '$15 ~ "0/1" ' | awk '$16 ~ "0/1" ' | awk '$17 ~ "0/1" ' | awk '$18 ~ "0/1" ' | awk '$19 ~ "0/1" ' | awk '$20 ~ "0/1" ' | awk '$21 ~ "0/1" ' | awk '$22 ~ "0/1" ' > 'H:\WORK\SEX-DET_Pmarg\WGS\results\GWAS\DP10_maf0.1_miss0_femHom_malHet_biallelic.vcf'
+
+
+
+#selecting heterozygous individuals allowing for the presence of missing data
+
+cat 'H:\WORK\SEX-DET_Pmarg\WGS\results\GWAS\sex_det_snp_DP10_maf0.1_miss0.9.final_biallelic.vcf' | awk '$10 ~ "0/1" || $10 == ".:.:.:.:.:.:.:.:." ' | awk '$11 ~ "0/1" || $11 == ".:.:.:.:.:.:.:.:." ' | awk '$13 ~ "0/1" || $13 == ".:.:.:.:.:.:.:.:." ' | awk '$14 ~ "0/1" || $14 == ".:.:.:.:.:.:.:.:." ' | awk '$15 ~ "0/1" || $15 == ".:.:.:.:.:.:.:.:." ' | awk '$16 ~ "0/1" || $16 == ".:.:.:.:.:.:.:.:." ' | awk '$17 ~ "0/1" || $17 == ".:.:.:.:.:.:.:.:." ' | awk '$18 ~ "0/1" || $18 == ".:.:.:.:.:.:.:.:." ' | awk '$19 ~ "0/1" || $19 == ".:.:.:.:.:.:.:.:." ' | awk '$20 ~ "0/1" || $20 == ".:.:.:.:.:.:.:.:." ' | awk '$21 ~ "0/1" || $21 == ".:.:.:.:.:.:.:.:." ' | awk '$22 ~ "0/1" || $22 == ".:.:.:.:.:.:.:.:." ' > 'H:\WORK\SEX-DET_Pmarg\WGS\results\GWAS\DP10_maf0.1_miss0.9_femHom_malHet_biallelic.vcf'
