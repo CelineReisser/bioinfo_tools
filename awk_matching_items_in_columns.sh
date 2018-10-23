b1f112070997afbbbb77a31090a831908abcf81c
cmor2207 <celine.reisser@gmail.com>
2018-10-22 22:16:45 -1000

    Create awk_matching_items_in_columns.sh

---
+++ awk_matching_items_in_columns.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+# Keep lines of file 2 matching the first column of file 1:
+
+file1=/path/to/your/item/to/select/in/one/column
+file2=/path/to/the/file/you/want/to/filter
+
+awk 'FNR==NR {a[$1]=$0; next}; $1 in a {print a[$1]}' file2 file1 > fitltered_file
+
