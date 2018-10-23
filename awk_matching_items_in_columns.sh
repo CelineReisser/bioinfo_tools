26da6490f7ba23cfdf779f9b9239efe723f412f2
cmor2207 <celine.reisser@gmail.com>
2018-10-22 22:25:13 -1000

    Update awk_matching_items_in_columns.sh

--- awk_matching_items_in_columns.sh
+++ awk_matching_items_in_columns.sh
@@ -2,7 +2,7 @@
 
 # Keep lines of file 2 matching the first column of file 1:
 
-file1=/path/to/your/item/to/select/in/one/column
+file1=/path/to/your/file/containing/motifs/you/wznt/to/keep
 file2=/path/to/the/file/you/want/to/filter
 
 awk 'FNR==NR {a[$1]=$0; next}; $1 in a {print a[$1]}' file2 file1 > fitltered_file
